d494f544859ccce913239463c7d429b7039d0f9b
David Johnson <david.j.c.johnson@gmail.com>
2017-02-04 12:18:13 -0500

    • + DatePickerTextField inputaccessoryview button

--- TwoP/DatePickerTextField.swift
+++ TwoP/DatePickerTextField.swift
@@ -45,10 +45,24 @@ class DatePickerTextField: UITextField {
         style = .time
         inputView = picker
         picker.backgroundColor = UIColor.white
-        picker.addTarget(self, action: #selector(datePickerDateDidChange(picker:)), for: .valueChanged)
+        picker.addTarget(self, action: #selector(datePickerDateDidChange(_:)), for: .valueChanged)
+      
+        let width = self.window?.frame.size.width ?? 0
+        let frame = CGRect(x: 0, y: 0, width: width, height: 50)
+        let button = UIButton(frame: frame)
+        button.backgroundColor = UIColor(red:0.38, green:0.62, blue:0.65, alpha:1.00)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.setTitle("Done", for: .normal)
+        button.addTarget(self, action: #selector(doneButtonWasTapped(_:)), for: .touchUpInside)
+      
+        inputAccessoryView = button
     }
     
-    @objc private func datePickerDateDidChange(picker: UIDatePicker) {
+    @objc private func datePickerDateDidChange(_ picker: UIDatePicker) {
         text = formatter.string(from: picker.date)
+    }
+  
+    @objc private func doneButtonWasTapped(_ sender: UIButton) {
+        resignFirstResponder()
     }
 }
