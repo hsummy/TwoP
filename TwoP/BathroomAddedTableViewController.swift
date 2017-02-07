3fca585cd00a21ad21b8265a3d62ef862f6a2c2b
Heather Summy <hsummy@hotmail.com>
2017-02-06 19:04:48 -0500

    user location - done. Bathroom Added VC in progress. Bathroom Detail VC layout begun.

--- TwoP/BathroomAddedTableViewController.swift
+++ TwoP/BathroomAddedTableViewController.swift
@@ -10,129 +10,139 @@ import UIKit
 import CoreData
 import CoreLocation
 
+enum Row: Int
+{
+    case dateAddedRow
+    case dateAddedPickerRow
+    case bathroomDescriptionRow
+    case flushRatingRow
+    case safeRow
+    case genderRow
+    case bathroomTypeRow
+    case handicapRow
+    case daysRow
+    case openingTimeRow
+    case openingTimePickerRow
+    case closingTimeRow
+    case closingTimePickerRow
+    case stallRow
+    case urinalRow
+    case changingTableRow
+    case extraNotesRow
+    case bathroomAddressRow
+    case latitudeRow
+    case longitudeRow
+    
+    
+    case unknown
+    
+    init(indexPath: IndexPath)
+    {
+        var row = Row.unknown
+        
+        switch (indexPath.section, indexPath.row)
+        {
+        case (0,0):
+            row = Row.dateAddedRow
+        case (0,1):
+            row = Row.dateAddedPickerRow
+        case (5,1):
+            row = Row.openingTimeRow
+        case (5,2):
+            row = Row.openingTimePickerRow
+        case (5,3):
+            row = Row.closingTimeRow
+        case (5,4):
+            row = Row.closingTimePickerRow
+        default: break
+        }
+
+        
+        self = row
+    }
+}
 
 
-class BathroomAddedTableViewController: UITableViewController, UITextFieldDelegate {
+class BathroomAddedTableViewController: UITableViewController, UITextFieldDelegate, CLLocationManagerDelegate {
 
     
-    @IBOutlet weak var dateAddedTextField: DatePickerTextField!
-    @IBOutlet weak var bathroomDescriptionText: UITableViewCell!
+    
+    @IBOutlet weak var dateAddedLabel: UILabel!
+    @IBOutlet weak var dateAddedPicker: UIDatePicker!
+    
+    @IBOutlet weak var bathroomDescriptionText: UITextField!
+    
+    @IBOutlet weak var flushRatingSegment: UISegmentedControl!
     
     
-    @IBOutlet weak var openingHourTextField: DatePickerTextField!
-    
-    @IBOutlet weak var closingHourTextField: DatePickerTextField!
     
     
+    
+    
+
+    @IBOutlet weak var MSUN: UIButton!
+    
+    @IBOutlet weak var openingHourTextField: UILabel!
+    @IBOutlet weak var openingHourTimePicker: UIDatePicker!
+    @IBOutlet weak var closingHourTextField: UILabel!
+    @IBOutlet weak var closingHourTimePicker: UIDatePicker!
+   
     @IBOutlet weak var stallCountAmtLabel: UILabel!
     @IBOutlet weak var stallStepper: UIStepper!
-
-    @IBOutlet weak var urinalCountAmt: UILabel!
+    @IBOutlet weak var urinalCountAmtLabel: UILabel!
     @IBOutlet weak var urinalStepper: UIStepper!
-
     
     @IBOutlet weak var extraNotesText: UITableViewCell!
     
 
-    @IBOutlet weak var bathroomAddress: UILabel!
-    @IBOutlet weak var latitude: UILabel!
-    @IBOutlet weak var longitude: UIView!
+    @IBOutlet weak var bathroomAddressDetail: UILabel!
+    @IBOutlet weak var latitudeDetail: UILabel!
+    @IBOutlet weak var longitudeDetail: UILabel!
+    
+    var datePickerHidden = false
     
     
+    var bathroomLocationInfo = [String: String]()
     
+    /* - ?????for days of the week buttons?
+ let calendar = NSCalendar.currentCalendar()
+ let date = NSDate()
+ let dateComponent =  calendar.components(NSCalendarUnit.CalendarUnitWeekday, fromDate: date)
+ let weekday = dateComponent.weekday
+ */
+ 
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
-        dateAddedTextField.style = .date
-        
-        let textFields = [
-            dateAddedTextField,
-            openingHourTextField,
-            closingHourTextField
-        ]
-        
-        for textField in textFields {
-            textField?.delegate = self
-        }
+        toggleDatePicker()
         
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
     }
     
-    func textFieldDidBeginEditing(_ textField: UITextField)
+    override func viewWillAppear(_ animated: Bool)
     {
-        <#code#>
+        super.viewWillAppear(animated)
+        bathroomAddressDetail.text = bathroomLocationInfo["address"]
+        latitudeDetail.text = bathroomLocationInfo["latitude"]
+        longitudeDetail.text = bathroomLocationInfo["longitude"]
+        
     }
+    
+
 
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
     }
-
-    // MARK: - Table view data source
-
-
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        textField.resignFirstResponder()
+        return false
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     // MARK: - Private
     func getContext() -> NSManagedObjectContext
@@ -155,25 +165,24 @@ class BathroomAddedTableViewController: UITableViewController, UITextFieldDelega
         
         
         //HNS - below hardcoded items wil be replaced with managedBathroomObject.setValue(gender, forKeyPath "gender") etc....
-        aBathroom.bathroomDescription = "The Iron Yard"
-        aBathroom.gender = "Male"
-        aBathroom.bathroomType = "Public"
+        // aBathroom.dateRated = picker
+        aBathroom.bathroomDescription = ""
+        aBathroom.flushRating = 4
+        aBathroom.safe = true
+        aBathroom.gender = ""
+        aBathroom.bathroomType = ""
         aBathroom.handicapAccess = true
         // aBathroom.openingTime = picker.date
-        
         // aBathroom.closingTime = picker
         //managedBathroomObject.setValue("Employees Only", forKey: "bathroomType")
-        aBathroom.flushRating = 4
-        // aBathroom.dateRated = picker
-        aBathroom.safe = true
-        // aBathroom.placemark = ?
-        aBathroom.extraNotes = "Clean most of the time. Some mishapes from time to time."
         aBathroom.stallCount = 0
         aBathroom.urinalCount = 0
         aBathroom.changingTable = true
-        aBathroom.latitude = -81.00
-        aBathroom.longitude = 23.00
-        aBathroom.bathroomAddress = "jdajf;d"
+        aBathroom.extraNotes = "Clean most of the time. Some mishapes from time to time."
+        aBathroom.bathroomAddress = bathroomLocationInfo["address"]
+        aBathroom.latitude = Double(bathroomLocationInfo["latitude"]!)!
+        aBathroom.longitude = Double(bathroomLocationInfo["longitude"]!)!
+        // aBathroom.placemark = ?
         
         do{
             try context.save()
@@ -184,15 +193,128 @@ class BathroomAddedTableViewController: UITableViewController, UITextFieldDelega
         
     }
 
-//MARK: IBActions
+
     
-
-    @IBAction func stallStepperValueChanged(_ sender: UIStepper)
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
-        stallCountAmtLabel.text = String(sender.value)
-
-
+        let row = Row(indexPath: indexPath)
+        
+        if row == .dateAddedRow
+        {
+            toggleDatePicker()
+        }
     }
     
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        let row = Row(indexPath: indexPath)
+        
+        switch row
+        {
+        case .dateAddedPickerRow:
+            if datePickerHidden
+            {
+                return 0
+            }
+            else
+            {
+                return super.tableView(tableView, heightForRowAt: indexPath)
+            }
+        default: return super.tableView(tableView, heightForRowAt: indexPath)
+        }
+    }
+    
+    func toggleDatePicker()
+    {
+        datePickerHidden = !datePickerHidden
+        
+        // Forces tableview to update itself
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+    
+    
+   
+//MARK: IBActions - Bar Buttons
+    
+    @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem)
+    {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem)
+    {
+        //HNS !!!!!Need to add 'place pin X' when saved.
+        saveBathroomToCoreData()
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+//MARK: IBActions - Date and Time Pickers - Date and When can...
+    
+    @IBAction func didChangeDateAdded(_ sender: UIDatePicker)
+    {//convert this dateinto a string.
+        dateAddedLabel.text = DateFormatter.localizedString(from: dateAddedPicker.date, dateStyle: .short, timeStyle: .short)
+        
+    }
+
+//MARK: IBActions - How was it...
+    
+    @IBAction func flushRatingSegmentButton(_ sender: UISegmentedControl)
+    {
+        
+    }
+
+    
+    @IBAction func safeSwitch(_ sender: UISwitch)
+    {
+        
+    }
+    
+   //MARK: IBActions - Who can...
+    @IBAction func genderSegmentButton(_ sender: UISegmentedControl)
+    {
+        
+    }
+    
+    @IBAction func bathroomTypeSegmentButton(_ sender: UISegmentedControl)
+    {
+        
+    }
+    
+
+    @IBAction func handicapSegmentChanged(_ sender: UISegmentedControl)
+    {
+        
+    }
+    
+ //MARK: IBActions - BONUS
+    @IBAction func stallStepperValueChanged(_ sender: UIStepper)
+    {
+        _ = sender.superview
+        _ = Int16(sender.value)
+        stallCountAmtLabel.text = String(sender.value)
+    }
+    
+
+    @IBAction func urinalStepperValueChanged(_ sender: UIStepper)
+    {
+        _ = sender.superview
+        _ = Int16(sender.value)
+        urinalCountAmtLabel.text = String(sender.value)
+    }
+    
+    @IBAction func changingTableSwitch(_ sender: UISwitch)
+    {
+        
+    }
+
+//MARK: IBActions - Notes
+    
+    
+    @IBAction func extraNotes(_ sender: UITextField)
+    {
+        dismiss(animated: true, completion: nil)
+    }
 
 }
